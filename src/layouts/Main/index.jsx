d772458e0200e760be7f584b841aa60a3c4efc94
rubicon44 <rub.b.c44@gmail.com>
2024-08-07 19:31:27 +0900

    [Add]Mainのレイアウトを追加

---
+++ src/layouts/Main/index.jsx
@@ -0,0 +1,7 @@
+import { BaseMain } from 'src/layouts/Main/style';
+
+export const Main = ({ children }) => (
+  <BaseMain>
+    {children}
+  </BaseMain>
+);
---
+++ src/layouts/Main/style.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const BaseMain = styled.main`
+  width: 100%;
+  padding: 50px;
+`;
